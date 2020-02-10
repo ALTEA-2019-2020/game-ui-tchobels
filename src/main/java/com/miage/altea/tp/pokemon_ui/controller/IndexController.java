7adaa9e7ecfd4163014684d62b06d78968276706
Alexis <alexis.bels@outlook.fr>
2020-02-10 10:30:01 +0100

    RELEASE : Livraison sur heroku

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.miage.altea.tp.pokemon_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +13,13 @@ public class IndexController {
     @GetMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping(value = "/registerTrainer")
+    ModelAndView registerNewTrainer(String trainerName){
+        var model = new ModelAndView();
+        model.setViewName("register");
+        model.addObject("name", trainerName);
+        return model;
     }
 }
